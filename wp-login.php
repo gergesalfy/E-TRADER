cdc45a7c26b29974f19940a6ddda04763bedeb52
Gerges Alfy <90783065+gergesalfy@users.noreply.github.com>
2023-03-08 22:45:15 +0200

    E-TRADER

---
+++ wp-login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://gergesalfy.github.io/E-TRADER/login/?http://simply_static_page=51749">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://gergesalfy.github.io/E-TRADER/login/?http://simply_static_page=51749";
+		</script>
+
+		<p>You are being redirected to <a href="https://gergesalfy.github.io/E-TRADER/login/?http://simply_static_page=51749">https://gergesalfy.github.io/E-TRADER/login/?http://simply_static_page=51749</a></p>
+	</body>
+</html>
